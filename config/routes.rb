3d1a8fe1d15cf7306421a5f0f5e9355fdbf741a1
AmuzaDongo <dongoamuza@gmail.com>
2022-08-26 17:04:21 +0300

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  root "main#index"
+  match 'about', to: "main#about", via: :get
+  match 'hello', to: "main#hello", via: :get
+  get 'tasks/index'
+  get 'tasks/new'
+  get 'tasks/edit'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
